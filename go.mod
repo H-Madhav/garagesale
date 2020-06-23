7c199057e7887b0e1d203c04d17040185ccd5686
hari.mj <hari.mj@incedoinc.com>
2020-06-23 23:07:59 +0530

    server

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/madhav1993/garagesale
 
 go 1.13
+
+require (
+	github.com/GuiaBolso/darwin v0.0.0-20191218124601-fd6d2aa3d244
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.7.0
+)
--- main.go
+++ main.go
@@ -3,31 +3,20 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/madhav1993/garagesale/schema"
 )
-
-//==========================simple server implementation==============================
-
-// func main() {
-// 	// Convert the Echo function to a type that implements http.Handler
-// 	h := http.HandlerFunc(Echo)
-// 	fmt.Println("server is running on localhost: 8000")
-// 	if err := http.ListenAndServe("localhost:8000", h); err != nil {
-// 		log.Fatalf("error: listening and serving: %s", err)
-// 	}
-// }
-
-// // Echo is a basic HTTP Handler.
-// func Echo(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "You asked to %s %s\n", r.Method, r.URL.Path)
-// }
-
-//======================================================================================
 
 // ========================server implementaion with gracefull shutdown=================
 
@@ -35,6 +24,31 @@ func main() {
 
 	log.Printf("main: started")
 	defer log.Println("main : Completed")
+
+	db, err := openDB()
+	if err != nil {
+		log.Fatal("connection error", err)
+	}
+	defer db.Close()
+
+	flag.Parse()
+	switch flag.Arg(0) {
+	case "migrate":
+		if err := schema.Migrate(db); err != nil {
+			log.Println("error applying migrations", err)
+			os.Exit(1)
+		}
+		log.Println("Migrations complete")
+		return
+
+	case "seed":
+		if err := schema.Seed(db); err != nil {
+			log.Println("error seeding database", err)
+			os.Exit(1)
+		}
+		log.Println("Seed data complete")
+		return
+	}
 
 	api := http.Server{
 		Addr:         "localhost:8000",
@@ -74,6 +88,23 @@ func main() {
 			log.Fatalf("main : could not stop server gracefully : %v", err)
 		}
 	}
+}
+
+func openDB() (*sqlx.DB, error) {
+	q := url.Values{}
+	q.Set("sslmode", "disable")
+	q.Set("timezone", "utc")
+
+	u := url.URL{
+		Scheme:   "postgresql",
+		User:     url.UserPassword("postgres", "postgres"),
+		Host:     "localhost",
+		Path:     "postgres",
+		RawQuery: q.Encode(),
+	}
+	fmt.Println(u.String())
+
+	return sqlx.Open("postgres", u.String())
 }
 
 //Product schema
